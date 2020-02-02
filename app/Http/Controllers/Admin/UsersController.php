4bc2529fb51c0bf105e18631cb12f0667d2874ff
Jeroen de Bruijn <jeroen@alphega.nl>
2020-02-02 09:46:42 +0100

    Initial commit (up to EP8)

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Role;
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index')->with('users', $users);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function edit(User $user)
+    {
+        $roles = Role::all();
+
+        return view('admin.users.edit')->with([
+            'user' => $user,
+            'roles' => $roles,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function update(Request $request, User $user)
+    {
+        $user->roles()->sync($request->roles);
+
+        return redirect()->route('admin.users.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function destroy(User $user)
+    {
+        $user->roles()->detach();
+        $user->delete();
+
+        return redirect()->route('admin.users.index');
+    }
+}
